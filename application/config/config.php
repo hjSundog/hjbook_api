6d379d35d7b3d3f41339b24d020027924472e0b7
lwio <443474713@qq.com>
2017-06-11 16:30:32 +0800

    server config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8084/';
+$config['base_url'] = 'http://lw-ezio.com/hjbook_api/';
 
 /*
 |--------------------------------------------------------------------------
